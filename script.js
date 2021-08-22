7bbded014d029312a315803528813e67b6c59af8
sahumo <7523460@gmail.com>
2021-08-22 15:47:02 +0300

    Замена  длинной записи циклом

--- script.js
+++ script.js
@@ -11,13 +11,16 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+}
+
 
 console.log(personalMovieDB);
 
